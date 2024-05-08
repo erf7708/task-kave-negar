00a2ca392e0f9f793111fac427745cc16dcb2993
ErfanRezaei <Erfan.rezaei2883@gmail.com>
2024-05-08 10:56:51 +0330

    Add notistack to the project to display messages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import "../styles/globals.css";
 import { queryClient } from "@/utils/queryClient";
 import { QueryClientProvider } from "react-query";
 import { PostProvider } from "@/context/postContext";
+import { SnackbarProvider } from "notistack";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <PostProvider>
-      <Component {...pageProps} />
+        <SnackbarProvider>
+          <Component {...pageProps} />
+        </SnackbarProvider>
       </PostProvider>
     </QueryClientProvider>
   );
